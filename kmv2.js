565f128fef6c67032f591b08d5466a5035e50056
Alex <aamlima123@gmail.com>
2015-10-16 09:38:25 -0300

    Added token auth

--- kmv2.js
+++ kmv2.js
@@ -48,6 +48,7 @@ var kmv2 = {
         div.appendChild(kmv2.qtd);
         kmv2.resultDiv = div;
         kmv2.gamesCodex = Ramen.getCollection("Games");
+        return kmv2.SetHeaders();
     },
     DestroySelf: function () {
         document.body.removeChild(kmv2.menuDiv);
@@ -81,12 +82,59 @@ var kmv2 = {
     onHistoryLoad: function (d, c) {
         kmv2.games = kmv2.games.concat(c.games.games);
         kmv2.update.disabled = false;
-        kmv2.start.setAttribute("value", (kmv2.games.length/kmv2.gamesCodex.pager.total)*100 + "%");
+        kmv2.start.setAttribute("value", (kmv2.games.length / kmv2.gamesCodex.pager.total) * 100 + "%");
+        Codex.util.ACS.destroyPromise(d);
     },
     onPromiseError: function (c, a, b) {
-        console.log("Error kmv2 onPromiseError");
-        console.log(a);
-        console.log(b);
+        console.log("Error kmv2 onPromiseError - " + a + " - " + b);
+        kmv2.start.disabled = false;
+        kmv2.start.setAttribute("value", "Pegar partidas");
+        Codex.util.ACS.destroyPromise(c);
+    },
+    SetHeaders: function () {
+        
+        var token = prompt("Insira o token: ");
+        if (token === "") return false;
+
+        var user = Codex.model.CurrentUser,
+        vaporRegion = user.getVaporRegion(),
+        header;
+
+        header = {
+            Region: vaporRegion
+        };
+
+        header.Authorization = 'Vapor ' + token;
+
+        Codex.util.ACS.setHeaders(header);
+
+        user = Codex.getContextUser();
+        var region = user.get('region');
+        var id = user.get('id');
+        var startIndex = 5;
+        var endIndex = 15;
+        var queryString = '?begIndex=' + startIndex + '&endIndex=' + endIndex + '&' + kmv2.gamesCodex.getFilters();
+        var url = Codex.util.ACS.getPlayerHistory(region, id, queryString);
+
+        Codex.util.ACS.makeRequest({
+            url: url,
+            listeners: {
+                load: kmv2.onHeaderUpdate,
+                error: kmv2.onPromiseError,
+                scope: kmv2
+            }
+        });
+        kmv2.start.disabled = true;
+        return true;
+    },
+    onHeaderUpdate: function (d, c) {
+        kmv2.start.disabled = false;
+        kmv2.start.setAttribute("value", "Pegar partidas");
+        kmv2.gamesCodex.readPager(c.games);
+        Codex.util.ACS.destroyPromise(d);
+    },
+    GetToken: function () {
+        return Codex.model.CurrentUser.getToken();
     }
 };
 
