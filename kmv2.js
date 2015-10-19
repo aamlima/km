82e7a228ae233bee39eea20c80c5640581040e07
Alex <aamlima123@gmail.com>
2015-10-19 09:50:29 -0300

    Modified game link

--- kmv2.js
+++ kmv2.js
@@ -19,13 +19,13 @@ var kmv2 = {
             var championAsc = Riot.DDragon.models.champion.remapKeys[gameAsc.participants[0].championId];
             var championDesc = Riot.DDragon.models.champion.remapKeys[gameDesc.participants[0].championId];
             kmv2.asc.innerHTML +=
-                "<a style=\"color: white;\" href=\"#match-details/BR1/" + gameAsc.gameId +
+                "<a style=\"color: white;\" href=\"#match-details/BR1/" + gameAsc.gameId + "/" + gameAsc.participantIdentities[0].player.currentAccountId +
                 "\" title=\"" + (gameAsc.participants[0].stats.win ? "V" : "D") + ": " + championAsc + "(" + gameAsc.participants[0].stats.champLevel + ") " +
                 gameAsc.participants[0].stats.kills + "/" + gameAsc.participants[0].stats.deaths + "/" + gameAsc.participants[0].stats.assists + "\">"
                 + championAsc[0] +
                 "</a>";
             kmv2.desc.innerHTML +=
-                "<a style=\"color: white;\" href=\"#match-details/BR1/" + gameDesc.gameId +
+                "<a style=\"color: white;\" href=\"#match-details/BR1/" + gameDesc.gameId + "/" + gameDesc.participantIdentities[0].player.currentAccountId +
                 "\" title=\"" + (gameDesc.participants[0].stats.win ? "V" : "D") + ": " + championDesc + "(" + gameDesc.participants[0].stats.champLevel + ") " +
                 gameDesc.participants[0].stats.kills + "/" + gameDesc.participants[0].stats.deaths + "/" + gameDesc.participants[0].stats.assists + "\">"
                 + championDesc[0] +
